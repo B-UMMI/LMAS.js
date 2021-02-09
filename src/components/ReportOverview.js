e53f209142be00cb76da76b50a826bdbd180804d
cimendes <iines.mendes@gmail.com>
2021-02-09 17:52:54 +0000

    add titles to report divs

--- src/components/ReportOverview.js
+++ src/components/ReportOverview.js
@@ -3,8 +3,6 @@ import React, { Component } from "react";
 import Aux from "../hoc/Aux";
 import classesRE from "./ReportOverview.css";
 import LamaLogo from "../images/lama.jpg";
-import mockStaph from "../data/mockSample_Staphylococcus_aureus_breadth_of_coverage_plot.json";
-import mockGlobal from "../data/mockSample_contig_size_distribution.json";
 
 // Material-UI ExpansionPanel components
 import Accordion from "@material-ui/core/Accordion";
@@ -36,49 +34,11 @@ import Button from "@material-ui/core/Button";
 import Typography from "@material-ui/core/Typography";
 import { withStyles } from "@material-ui/core/styles";
 
+// Material UI Progress Circle
+import CircularProgress from '@material-ui/core/CircularProgress';
+
 // Plotly.js
 import Plot from "react-plotly.js";
-
-
-const createData3 = (
-  reference,
-  contiguity,
-  identity,
-  lIdentity,
-  breadth,
-  c90,
-  c95,
-  alignedC,
-  na50,
-  alignedBp
-) => {
-  return {
-    reference,
-    contiguity,
-    identity,
-    lIdentity,
-    breadth,
-    c90,
-    c95,
-    alignedC,
-    na50,
-    alignedBp,
-  };
-};
-
-const rows3 = [
-  createData3("BCALM2", 159, 6.0, 24, 4.0, 1.0, 5.0, 8, 2, 3),
-  createData3("GATBMiniaPipeline", 237, 9.0, 37, 4.3, 1.0, 5.0, 8, 2, 3),
-  createData3("IDBA-UD", 262, 16.0, 24, 6.0, 1.0, 5.0, 8, 2, 3),
-  createData3("MEGAHIT", 305, 3.7, 67, 4.3, 1.0, 5.0, 8, 2, 3),
-  createData3("metaSPAdes", 356, 16.0, 49, 3.9, 1.0, 5.0, 8, 2, 3),
-  createData3("minia", 356, 16.0, 49, 3.9, 1.0, 5.0, 8, 2, 3),
-  createData3("PANDAseq", 356, 16.0, 49, 3.9, 1.0, 5.0, 8, 2, 3),
-  createData3("SKESA", 356, 16.0, 49, 3.9, 1.0, 5.0, 8, 2, 3),
-  createData3("SPAdes", 356, 16.0, 49, 3.9, 1.0, 5.0, 8, 2, 3),
-  createData3("unicycler", 356, 16.0, 49, 3.9, 1.0, 5.0, 8, 2, 3),
-  createData3("VelvetOptimizer", 356, 16.0, 49, 3.9, 1.0, 5.0, 8, 2, 3),
-];
 
 const style = {
   buttonBar: {
@@ -136,6 +96,7 @@ class ReportOverview extends Component {
   };
 
   render() {
+    
     const { classes } = this.props;
 
     const referenceFile = Object.keys(this.state.referenceData)[0]
@@ -186,6 +147,11 @@ class ReportOverview extends Component {
 
     const globalTable = (
       <TableContainer component={Paper}>
+      <div style={{ marginTop: "20px" }}>
+        <Typography  variant="h5" align='center' style={{ color: "#4A690C" }}>
+              <b>{this.state.buttonOption}</b>
+        </Typography>
+      </div>
         <Table
           className={classes.table}
           size="small"
@@ -238,7 +204,7 @@ class ReportOverview extends Component {
             </TableRow>
           </TableHead>
           <TableBody>
-            {this.state.mainDataTables.mockSample.GlobalTable.map((row) => (
+            {this.state.mainDataTables[this.state.buttonOption].GlobalTable.map((row) => (
               <TableRow key={row.assembler}>
                 <TableCell component="th" scope="row">
                   {row.assembler}
@@ -247,7 +213,7 @@ class ReportOverview extends Component {
                 <TableCell align="right">{row.original.basepairs}</TableCell>
                 <TableCell align="right">{row.original.max_contig_size}</TableCell>
                 <TableCell align="right">{row.original.N50}</TableCell>
-                <TableCell align="right">{row.original.mapped_reads}</TableCell>
+                <TableCell align="right">{row.original.mapped_reads.toFixed(4)}</TableCell>
                 <TableCell align="right">{row.filtered.contigs}</TableCell>
                 <TableCell align="right">{row.filtered.basepairs}</TableCell>
                 <TableCell align="right">{row.filtered.N50}</TableCell>
@@ -332,8 +298,8 @@ class ReportOverview extends Component {
         >
           <Grid item xs={6}>
             <Plot
-              data={this.state.mainPlotData.mockSample.PlotData[this.state.dropdownOption][0].data}
-              layout={this.state.mainPlotData.mockSample.PlotData[this.state.dropdownOption][0].layout}
+              data={this.state.mainPlotData[this.state.buttonOption].PlotData[this.state.dropdownOption][0].data}
+              layout={this.state.mainPlotData[this.state.buttonOption].PlotData[this.state.dropdownOption][0].layout}
               useResizeHandler={true}
               style={{ width: "100%", height: "100%" }}
               line={{
@@ -343,8 +309,8 @@ class ReportOverview extends Component {
           </Grid>
           <Grid item xs={6}>
             <Plot
-              data={this.state.mainPlotData.mockSample.PlotData[this.state.dropdownOption][1].data}
-              layout={this.state.mainPlotData.mockSample.PlotData[this.state.dropdownOption][1].layout}
+              data={this.state.mainPlotData[this.state.buttonOption].PlotData[this.state.dropdownOption][1].data}
+              layout={this.state.mainPlotData[this.state.buttonOption].PlotData[this.state.dropdownOption][1].layout}
               useResizeHandler={true}
               style={{ width: "100%", height: "100%" }}
               line={{
@@ -359,8 +325,8 @@ class ReportOverview extends Component {
     const gridPlotWide = (
       <div style={{ marginTop: "20px" }}>
         <Plot
-          data={this.state.mainPlotData.mockSample.PlotData[this.state.dropdownOption][2].data}
-          layout={this.state.mainPlotData.mockSample.PlotData[this.state.dropdownOption][2].layout}
+          data={this.state.mainPlotData[this.state.buttonOption].PlotData[this.state.dropdownOption][2].data}
+          layout={this.state.mainPlotData[this.state.buttonOption].PlotData[this.state.dropdownOption][2].layout}
           useResizeHandler={true}
           style={{ width: "100%", height: "100%" }}
           line={{
@@ -387,14 +353,17 @@ class ReportOverview extends Component {
 
       const refComponent = (
         <div style={{ marginTop: "20px" }}>
+          <Typography  variant="h5" align='center' style={{ color: "#4A690C" }}>
+            <b>{this.state.buttonOption}</b>
+          </Typography>
           <Typography variant="h6" style={{ color: "#4A690C" }}>
             {this.state.dropdownOption}
           </Typography>
           <Typography variant="subtitle1">
-            <b>Size:</b> {this.state.referenceData[referenceFile][this.state.dropdownOption].size} Basepairs
+            <b>Size:</b> {(this.state.referenceData[referenceFile][this.state.dropdownOption].size / 1000000).toFixed(2)} Mega Basepairs
           </Typography>
           <Typography variant="subtitle1">
-            <b>%GC:</b> {this.state.referenceData[referenceFile][this.state.dropdownOption].GC}
+            <b>%GC:</b> {this.state.referenceData[referenceFile][this.state.dropdownOption].GC.toFixed(2)}
           </Typography>
           <div style={{ marginTop: "20px" }}>
             <TableContainer component={Paper}>
@@ -441,16 +410,16 @@ class ReportOverview extends Component {
                   </TableRow>
                 </TableHead>
                 <TableBody>
-                  {this.state.mainDataTables.mockSample.ReferenceTables[this.state.dropdownOption][0].map((row) => (
+                  {this.state.mainDataTables[this.state.buttonOption].ReferenceTables[this.state.dropdownOption][0].map((row) => (
                     <TableRow key={row.assembler}>
                       <TableCell component="th" scope="row">
                         {row.assembler}
                       </TableCell>
-                      <TableCell align="right">{row.contiguity}</TableCell>
-                      <TableCell align="right">{row.multiplicity}</TableCell>
-                      <TableCell align="right">{row.identity}</TableCell>
-                      <TableCell align="right">{row.lowest_identity}</TableCell>
-                      <TableCell align="right">{row.breadth_of_coverage}</TableCell>
+                      <TableCell align="right">{row.contiguity.toFixed(4)}</TableCell>
+                      <TableCell align="right">{row.multiplicity.toFixed(4)}</TableCell>
+                      <TableCell align="right">{row.identity.toFixed(4)}</TableCell>
+                      <TableCell align="right">{row.lowest_identity.toFixed(4)}</TableCell>
+                      <TableCell align="right">{row.breadth_of_coverage.toFixed(4)}</TableCell>
                       <TableCell align="right">{row.L90}</TableCell>
                       <TableCell align="right">{row.aligned_contigs}</TableCell>
                       <TableCell align="right">{row.NA50}</TableCell>
@@ -476,8 +445,8 @@ class ReportOverview extends Component {
           >
             <Grid item xs={6}>
               <Plot
-                data={this.state.mainPlotData.mockSample.PlotData[this.state.dropdownOption][0][0].data}
-                layout={this.state.mainPlotData.mockSample.PlotData[this.state.dropdownOption][0][0].layout}
+                data={this.state.mainPlotData[this.state.buttonOption].PlotData[this.state.dropdownOption][0][0].data}
+                layout={this.state.mainPlotData[this.state.buttonOption].PlotData[this.state.dropdownOption][0][0].layout}
                 useResizeHandler={true}
                 style={{ width: "100%", height: "100%" }}
                 line={{
@@ -487,8 +456,8 @@ class ReportOverview extends Component {
             </Grid>
             <Grid item xs={6}>
               <Plot
-                data={this.state.mainPlotData.mockSample.PlotData[this.state.dropdownOption][1][0].data}
-                layout={this.state.mainPlotData.mockSample.PlotData[this.state.dropdownOption][1][0].layout}
+                data={this.state.mainPlotData[this.state.buttonOption].PlotData[this.state.dropdownOption][1][0].data}
+                layout={this.state.mainPlotData[this.state.buttonOption].PlotData[this.state.dropdownOption][1][0].layout}
                 useResizeHandler={true}
                 style={{ width: "100%", height: "100%" }}
                 line={{
@@ -511,8 +480,8 @@ class ReportOverview extends Component {
           >
             <Grid item xs={6}>
               <Plot
-                data={this.state.mainPlotData.mockSample.PlotData[this.state.dropdownOption][2][0].data}
-                layout={this.state.mainPlotData.mockSample.PlotData[this.state.dropdownOption][2][0].layout}
+                data={this.state.mainPlotData[this.state.buttonOption].PlotData[this.state.dropdownOption][2][0].data}
+                layout={this.state.mainPlotData[this.state.buttonOption].PlotData[this.state.dropdownOption][2][0].layout}
                 useResizeHandler={true}
                 style={{ width: "100%", height: "100%" }}
                 line={{
@@ -522,8 +491,8 @@ class ReportOverview extends Component {
             </Grid>
             <Grid item xs={6}>
               <Plot
-                data={this.state.mainPlotData.mockSample.PlotData[this.state.dropdownOption][3][0].data}
-                layout={this.state.mainPlotData.mockSample.PlotData[this.state.dropdownOption][3][0].layout}
+                data={this.state.mainPlotData[this.state.buttonOption].PlotData[this.state.dropdownOption][3][0].data}
+                layout={this.state.mainPlotData[this.state.buttonOption].PlotData[this.state.dropdownOption][3][0].layout}
                 useResizeHandler={true}
                 style={{ width: "100%", height: "100%" }}
                 line={{
